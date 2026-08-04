ee289907f02132bd31aba2e838368f9d20b2e572
Can Kurt <cankurtcvr@gmail.com>
2026-08-05 01:43:18 +0200

    Import CVDetail component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CVDetail from "./pages/CVDetail";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
